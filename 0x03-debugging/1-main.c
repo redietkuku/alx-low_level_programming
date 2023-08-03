467ee54c937286856ce410329dbd6b632b7f070e
redietkuku <redietatsede@gmail.com>
2023-08-03 04:26:17 -0700

    fixed line count error

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,10 +3,6 @@
 /**
  * main - Entry point of the program.
  *
- * This function causes an infinte loop, but it's commented out.
- * It prints a message indicating an infinte loop, and then a message
- * about avoiding the infinite loop.
- *
  * Return: 0 on success.
  */
 int main(void)
@@ -16,14 +12,12 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
 	/*
 	* while (i < 10)
 	* {
 	*     putchar(i);
 	* }
 	*/
-
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
