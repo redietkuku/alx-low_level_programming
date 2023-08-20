6df5adf68a4d09e9fb1d724e92eaa5eb714f69e4
redietkuku <redietatsede@gmail.com>
2023-08-19 20:59:47 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+/**
+ * reverse_array - reverses array
+ * @a: array to be reversed
+ * @n: number of elements in array
+ *
+ * Retrun : Always 0.
+ */
+void reverse_array(int *a, int n)
+{
+	int x, y;
+
+	for (x = 0; (x < (n - 1) / 2); x++)
+	{
+		y = a[x];
+		a[x] = a[n - 1 - x];
+		a[n - 1 - x] = c;
+	}
+}
