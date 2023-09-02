a227f49fb0f6925f0fd9e2c73724a456448e2ca9
redietkuku <redietatsede@gmail.com>
2023-09-01 20:45:59 -0700

    fixed betty style issue

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,32 +1,21 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
  *
  * Return: the int converted from the string
  */
-
 int _atoi(char *s)
-
 {
-
 	int i, d, n, len, f, digit;
 
-
 	i = 0;
-
 	d = 0;
-
 	n = 0;
-
 	len = 0;
-
 	f = 0;
-
 	digit = 0;
-
 
 	while (s[len] != '\0')
 	{
@@ -37,7 +26,6 @@ int _atoi(char *s)
 	{
 		if (s[i] == '-')
 			++d;
-
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
@@ -47,25 +35,20 @@ int _atoi(char *s)
 			}
 			n = n * 10 + digit;
 			f = 1;
-
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 			{
 				break;
 			}
 			f = 0;
-
 		}
-
 		i++;
 	}
-
 	if (f == 0)
 	{
 		return (0);
 	}
 	return (n);
 }
-
 /**
  * main - multiplis two numbers
  * @argc: number of arguments
