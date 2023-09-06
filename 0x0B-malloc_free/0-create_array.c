406922a09940d352488c858bd9e61b553fd798db
redietkuku <redietatsede@gmail.com>
2023-09-06 14:07:32 -0700

    Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of characters and intializes it
+ * with a specific char
+ * @size: size of the array
+ * @c: intial integer value
+ *
+ * Return: a pointer if success and NULL if fail
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int x;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	arr = (char *)malloc(sizeof(char) * size);
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (x = 0; x < size; x++)
+	{
+		arr[x] = c'
+	}
+
+	return (arr);
+}
