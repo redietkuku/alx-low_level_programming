2e91451b29e26e99d2d33576de9244de1f2d010e
redietkuku <redietatsede@gmail.com>
2023-09-12 19:05:12 -0700

    added description for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef _DOG_DETAILS
 #define _DOG_DETAILS
 
+/**
+ * struct dog - a new data type that contains details of a dog
+ * @name: string value for name of the dog
+ * @age: float value for age of the dog
+ * @owner: string value for name of the dog's owner
+ */
 struct dog
 {
 	char *name;
