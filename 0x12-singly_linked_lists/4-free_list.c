06c4cb3c0aa64345a3394c3054139eac0e0da52e
redietkuku <redietatsede@gmail.com>
2023-09-21 16:47:28 -0700

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: head node of list_t
+ *
+ * Return: Always 0
+ */
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		if (head-.str)
+		{
+			free(head->str);
+		}
+		free(head);
+	}
+}
