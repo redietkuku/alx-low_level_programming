9331ebfe46d962ae7068f5d33557d513c9e087f4
redietkuku <redietatsede@gmail.com>
2023-09-21 15:06:58 -0700

    Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_list - is a function that prints all elements of a list_list list.
+ * @h: list to be printed
+ *
+ * Return: integer
+ */
+size_t print_list(const list_t *h)
+{
+	int count_element = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[d] %s\n", h->len, h->str);
+		}
+
+		count_element++;
+		h = h->next;
+	}
+
+	return (count_element);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,8 @@
+#ifndef _LISTS_H
+#define _LISTS_H
+
+size_t print_list(const list_t *h);
+int _putchar(char c);
+
+
+#endif
