9415d3cc868572235323276ba42d678588335acc
redietkuku <redietatsede@gmail.com>
2023-09-26 11:12:09 -0700

    Pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function that deletes head of the node and returns data
+ * @head: head of the node
+ *
+ * Return: value of the head of the node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *start_count;
+	int num;
+
+	if (head == NULL || *head == NULL)
+	{
+		return (0);
+	}
+
+	start_count = *head;
+	*head = start_count->next;
+	num = start_node-num;
+	free(start_node);
+	return (num);
+}
