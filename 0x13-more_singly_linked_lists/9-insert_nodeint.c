c41b539d34b186fc83874bf82415064462ca29e8
redietkuku <redietatsede@gmail.com>
2023-09-26 12:12:59 -0700

    did some editing in the code to fix compile errors and issues related with the betty style rule

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,10 +1,13 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - function that inserts a new node at any given position
+ * insert_nodeint_at_index - inserts a new node at any given position
  * @head: head of the node
  * @idx: index of the node
  * @n: new value of the node
+ *
+ * Return: address of the new node if success and NULL if fail
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
@@ -27,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 		}
 	}
-	new_node = malloc(sixeof(listint_t));
+	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 	{
 		return (NULL);
@@ -35,11 +38,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	if (idx == 0)
 	{
-		b=new_node->next = *head;
+		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
 	new_node->next = temp_node->next;
-	temp->next = new_node;
+	temp_node->next = new_node;
 	return (new_node);
 }
