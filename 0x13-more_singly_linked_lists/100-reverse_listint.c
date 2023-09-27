62c16c08ec436c4f23cfe27e02ee753ad1cddc72
redietkuku <redietatsede@gmail.com>
2023-09-26 17:23:27 -0700

    a semicolon goes a long way

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * reverse_listint - function that reverses listint_t
@@ -8,25 +10,26 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *precede, *next_node;
+	listint_t *previous_node, *next_node;
 
 	if (head == NULL || *head == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	if ((*head)->next == NULL)
 	{
 		return (*head);
 	}
-	precede = NULL;
+	previous_node = NULL;
+
 	while (*head != NULL)
 	{
 		next_node = (*head)->next;
-		(*head)->next = precede;
-		precede = *head;
+		(*head)->next = previous_node;
+		previous_node = *head;
 		*head = next_node;
 	}
-	*head = precede;
+	*head = previous_node;
 	return (*head);
 }
