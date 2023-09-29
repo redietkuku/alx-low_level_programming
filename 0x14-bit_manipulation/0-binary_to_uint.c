de88db731dfaf4bfa02c2ce8456257396b54409d
redietkuku <redietatsede@gmail.com>
2023-09-29 16:45:24 -0700

    fixed betty style issue and changed some lines of code to give the correct outputs

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,23 +13,24 @@ unsigned int binary_to_uint(const char *b)
 
 	y = 0;
 
-	if (y == false)
+	if (!b)
 	{
 		return (0);
 	}
-	for ( x = 0; b[x] != '\0'; x++)
+	for (x = 0; b[x] != '\0'; x++)
 	{
 		if (b[x] != '0' && b[x] != '1')
 		{
 			return (0);
 		}
 	}
-	for ( x = 0; b[x] != '\0'; x++)
+	for (x = 0; b[x] != '\0'; x++)
 	{
 		y <<= 1;
 		if (b[x] == '1')
 		{
 			y += 1;
 		}
+	}
 	return (y);
 }
