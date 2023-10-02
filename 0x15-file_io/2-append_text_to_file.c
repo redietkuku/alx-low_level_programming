5329e58118def99c049e2c7c4b5c8ad08c159372
redietkuku <redietatsede@gmail.com>
2023-10-02 16:15:03 -0700

    Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * _strlen - function that finds the length of a string
+ * @string: string pointer
+ *
+ * Return: length of a string
+ */
+size_t _strlen(char *string)
+{
+	size_t x;
+
+	for (x = 0; string[x]; x++)
+	{
+		;
+	}
+
+	return (x);
+}
+
+/**
+ * append_text_to_file - is a func that appends text to file
+ * @filename: filename pointer
+ * @text_content: NULL terminated string
+ *
+ * Return: 1 if success and -1 if fail
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int f;
+	ssize_t length;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	f = open(filename, o_WRONLY | O_APPEND);
+
+	if (f == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		length = write(f, text_content, _strlen(text_content));
+	}
+
+	close(f);
+	if (length == -1)
+	{
+		return (-1);
+	}
+
+	return (1);
+}
