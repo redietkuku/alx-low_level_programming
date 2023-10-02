98c70c32cf86786f09b1bccb627ea721e57909b4
redietkuku <redietatsede@gmail.com>
2023-10-02 16:17:35 -0700

    fixed spelling error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -34,7 +34,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	f = open(filename, o_WRONLY | O_APPEND);
+	f = open(filename, O_WRONLY | O_APPEND);
 
 	if (f == -1)
 	{
