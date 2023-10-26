13bdda3cba627d474bf915b2de018596b7749b7a
redietkuku <redietatsede@gmail.com>
2023-10-26 11:20:21 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * calc_power - calculates base and power of a number
+ * @b: base
+ * @p: power
+ *
+ * Return: an integer
+ */
+unsigned long int calc_power(unsigned int b, unsigned int p)
+{
+	unsigned long int number;
+	unsigned int x;
+
+	number = 1;
+	for (x = 1; x <= p; x++)
+	{
+		number *= b;
+	}
+	return (number);
+}
+
+/**
+ * print_binary - prints the binary from of a number
+ * @n: number that will be converted
+ *
+ * Return: Always 0
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int dividend, final_result;
+	char check;
+
+	check = 0;
+	dividend = clac_power(2, sizeof(unsigned long int) * 8 - 1);
+
+	while (dividend != 0)
+	{
+		final_result = n & dividend;
+		if (final_result == dividend)
+		{
+			check = 1;
+			_putchar( '1');
+		}
+		else if (check == 1 || dividend == 1)
+		{
+			_putchar('0');
+		}
+		dividend >>= 1;
+	}
+}
