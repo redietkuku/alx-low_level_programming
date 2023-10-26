eb6ed78caafba533e6eb5d95d02ac163e456ab61
redietkuku <redietatsede@gmail.com>
2023-10-26 11:50:54 -0700

    2-get.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * get_bit - returns value of bit at a given index
+ * @n: bit
+ * @index: given index
+ *
+ * Return: value of bit at index if success and -1 if fail
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int dividend, final_resluts;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+	{
+		return (-1);
+	}
+
+	dividend = 1 << index;
+	final_result = n & dividend;
+	if (final_reslut == dividend)
+	{
+		return (1);
+	}
+
+	return (0);
+}
