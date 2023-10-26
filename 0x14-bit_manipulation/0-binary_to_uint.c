fc390f348d7d82e29a3c966d0e7ba4f5de1a5e7c
redietkuku <redietatsede@gmail.com>
2023-10-26 10:16:40 -0700

    binary_to_uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#inlcude "main.h"
+
+/**
+ *  binary_to_uint - converts a binary number to an unsigned int
+ *  @b: string pointer
+ *
+ *  Return: integer if success or 0 if b is null
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int a;
+	unsigned int c;
+
+	c = 0;
+
+	if (!b)
+	{
+		return (0);
+	}
+	for (a = 0; b[a] != '\0'; x++)
+	{
+		if (b[a] != '0' && b[a] != '1')
+		{
+			return (0);
+		}
+	}
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		c <<= 1;
+		if (b[a] == '1')
+		{
+			c += 1;
+		}
+	}
+	return (c);
+}
