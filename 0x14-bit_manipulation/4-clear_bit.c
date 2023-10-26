fce83a65e1c42ec7685c45f9bf72c240de9cbdf4
redietkuku <redietatsede@gmail.com>
2023-10-26 12:20:20 -0700

    4-clear

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#inlcude "mian.h"
+
+/**
+ * clear_bit - sets value of bit ot 0 at a given index
+ * @n: bit
+ * @index: index
+ *
+ * Return: 1 if success and  -1 if fail
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int x;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+	{
+		return (-1);
+	}
+
+	x = ~(1 << index);
+	*n = *n & x;
+
+	return (1);
+}
