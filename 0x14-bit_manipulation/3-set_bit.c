2ff36700463b82fac3c5b2cce1f446828869a101
redietkuku <redietatsede@gmail.com>
2023-10-26 12:06:09 -0700

    3-set_bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * set_bit - sets value of bit to 1 ata given index
+ * @n: bit
+ * @index: index
+ *
+ * Return: 1 if success and -1 if fail
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int x;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+	{
+		return (-1);
+	}
+
+	x = 1 << index;
+	*n = *n | x;
+
+	return (1);
+}
