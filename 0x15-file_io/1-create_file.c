87b3102b00b793c49aafdbcdf756c8386211b22a
redietkuku <redietatsede@gmail.com>
2023-10-29 23:13:12 -0700

    fixed betty style and compile error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,8 +24,8 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 
-	opn_it = opne(filename, O_CREAT || O_RDWR | O_TRUNC, 0600);
-	wr_it = write(opn_it; text_content, length);
+	opn_it = open(filename, O_CREAT || O_RDWR | O_TRUNC, 0600);
+	wr_it = write(opn_it, text_content, length);
 
 	if (opn_it == -1 || wr_it == -1)
 	{
